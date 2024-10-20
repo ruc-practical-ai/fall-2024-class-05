e677dd6c5fdcc48425c7509f4af13782dc963915
Mauro J. Sanchirico III <sanchirico.mauro@gmail.com>
2024-10-20 19:06:42 +0000

    Add example nbia download and run all notebooks

---
+++ scripts/example_nbia_download.sh
@@ -0,0 +1,1 @@
+expect submodules/data-loaders/scripts/nbia_download.tcl DOWNLOAD -1 /workspaces/fall-2024-class-05/submodules/data-loaders/nbia_manifest_files/small_test_manifest/manifest-*.tcia /workspaces/fall-2024-class-05/submodules/data-loaders/downloads/nbia/
